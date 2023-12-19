4c5dd0efe93d2819009003ffcfeea821a87958c4
MaxiColombo <maximocolombo2003@gmail.com>
2023-12-19 13:56:52 -0300

    Changes verc

--- app.js
+++ app.js
@@ -1,29 +1,23 @@
-
-//npm install express
-const express = require('express')
-const app = express()
-
-const path = require("path");
-
-//Le decimos donde esta la ruta publica -- Usamos recursos estaticos
-app.use(express.static(path.join(__dirname, 'public')));
-
-
+const express = require('express');
+const path = require('path');
+const app = express();
 
 // Configura EJS como motor de plantillas
 app.set('view engine', 'ejs');
 
 // Configura la carpeta de vistas
-app.set('views', __dirname + '/src/views');
+app.set('views', path.join(__dirname, '/src/views'));
 
-// Rutas y lógica de tu aplicación
+// Configura la carpeta de archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta de ejemplo
 app.get('/', (req, res) => {
-  res.render('portfolio'); // Esto es un ejemplo, debes tener tus propias rutas y lógica
+  res.render('portfolio'); // Ajusta esto según tus necesidades
 });
 
-
-//Hacer correr el servidor
-app.listen(3320,() => {
-    console.log("Servidor Running http://localhost:3320");
+// Iniciar el servidor
+const port = process.env.PORT || 3320;
+app.listen(port, () => {
+  console.log(`Servidor iniciado en http://localhost:${port}`);
 });
-
